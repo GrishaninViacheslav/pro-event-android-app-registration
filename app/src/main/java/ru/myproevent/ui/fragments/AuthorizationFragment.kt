2a59bc424d50931fae65f1aaf0639e5bfd65d9c5
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-10-18 18:46:40 +0300

    @wip

--- app/src/main/java/ru/myproevent/ui/fragments/AuthorizationFragment.kt
+++ app/src/main/java/ru/myproevent/ui/fragments/AuthorizationFragment.kt
@@ -1,13 +1,12 @@
 package ru.myproevent.ui.fragments
 
 import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
 import ru.myproevent.R
@@ -20,6 +19,9 @@ import ru.myproevent.ui.presenters.authorization.AuthorizationView
 class AuthorizationFragment : MvpAppCompatFragment(), AuthorizationView, BackButtonListener {
     private var _view: FragmentAuthorizationBinding? = null
     private val view get() = _view!!
+
+    private var emailInvalidError = false
+    private var passwordInvalidError = false
 
     private val presenter: AuthorizationPresenter by moxyPresenter {
         AuthorizationPresenter()
@@ -37,42 +39,76 @@ class AuthorizationFragment : MvpAppCompatFragment(), AuthorizationView, BackBut
             authorizationConfirm.setOnClickListener {
                 presenter.authorize(emailEdit.text.toString(), passwordEdit.text.toString())
             }
-            emailInput.errorIconDrawable = null
-            passwordInput.errorIconDrawable = null
+            val colorState = ColorStateList(
+                arrayOf(
+                    intArrayOf(android.R.attr.state_active),
+                    intArrayOf(android.R.attr.state_focused),
+                    intArrayOf(-android.R.attr.state_focused),
+                    intArrayOf(android.R.attr.state_hovered),
+                    intArrayOf(android.R.attr.state_enabled),
+                    intArrayOf(-android.R.attr.state_enabled)
+                ),
+                intArrayOf(
+                    requireContext().getColor(R.color.PE_blue_gray_04),
+                    requireContext().getColor(R.color.PE_blue_gray_03),
+                    requireContext().getColor(R.color.PE_blue_gray_03),
+                    requireContext().getColor(R.color.PE_blue_gray_04),
+                    requireContext().getColor(R.color.PE_blue_gray_04),
+                    requireContext().getColor(R.color.PE_blue_gray_04)
+                )
+            )
+            emailEdit.setOnFocusChangeListener { v, hasFocus ->
+                if (!hasFocus) {
+                    emailInvalidError = false
+                    emailInput.setBoxStrokeColorStateList(colorState)
+
+                    if(!passwordInvalidError){
+                        view.errorMessage.visibility = GONE
+                    }
+                }
+            }
+            passwordEdit.setOnFocusChangeListener { v, hasFocus ->
+                if (!hasFocus) {
+                    passwordInvalidError = false
+                    passwordInput.setBoxStrokeColorStateList(colorState)
+
+                    if(!emailInvalidError){
+                        view.errorMessage.visibility = GONE
+                    }
+                }
+            }
         }
         return view.root
     }
 
-    private var flag = true
 
     override fun authorizationDataInvalid() {
         with(view) {
-//            emailInput.boxStrokeColor = requireContext().getColor(R.color.PE_bright_red)
-//            passwordInput.boxStrokeColor = requireContext().getColor(R.color.PE_bright_red)
-
-            flag = if (flag) {
-                val colorState = ColorStateList(
-                    arrayOf(intArrayOf(android.R.attr.state_active),
-                        intArrayOf( android.R.attr.state_focused),
-                        intArrayOf( -android.R.attr.state_focused),
-                        intArrayOf( android.R.attr.state_hovered),
-                        intArrayOf( android.R.attr.state_enabled),
-                        intArrayOf(- android.R.attr.state_enabled)),
-                    intArrayOf(
-                        requireContext().getColor(R.color.PE_bright_red),
-                        requireContext().getColor(R.color.PE_blue_gray_03),
-                        requireContext().getColor(R.color.PE_blue_gray_03),
-                        requireContext().getColor(R.color.PE_bright_red),
-                        requireContext().getColor(R.color.PE_bright_red),
-                        requireContext().getColor(R.color.PE_bright_red))
+            val colorState = ColorStateList(
+                arrayOf(
+                    intArrayOf(android.R.attr.state_active),
+                    intArrayOf(android.R.attr.state_focused),
+                    intArrayOf(-android.R.attr.state_focused),
+                    intArrayOf(android.R.attr.state_hovered),
+                    intArrayOf(android.R.attr.state_enabled),
+                    intArrayOf(-android.R.attr.state_enabled)
+                ),
+                intArrayOf(
+                    requireContext().getColor(R.color.PE_bright_red),
+                    requireContext().getColor(R.color.PE_blue_gray_03),
+                    requireContext().getColor(R.color.PE_blue_gray_03),
+                    requireContext().getColor(R.color.PE_bright_red),
+                    requireContext().getColor(R.color.PE_bright_red),
+                    requireContext().getColor(R.color.PE_bright_red)
                 )
-                emailInput.setBoxStrokeColorStateList(colorState)
-                passwordInput.error = getString(R.string.authorization_data_invalid)
-                false
-            } else {
-                passwordInput.isErrorEnabled = false
-                true
-            }
+            )
+            emailInvalidError = true
+            passwordInvalidError = true
+            emailInput.setBoxStrokeColorStateList(colorState)
+            passwordInput.setBoxStrokeColorStateList(colorState)
+            errorMessage.text = getString(R.string.authorization_data_invalid)
+            errorMessage.visibility = VISIBLE
+            false
         }
     }
 
